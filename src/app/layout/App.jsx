627d0306c01cb6981fb1387e899edf6936a13202
sdlfkorg <sdlfkorg@gmail.com>
2018-10-01 21:08:57 +0800

    feature: eventdashboard layout

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {Container} from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+
+
+
 class App extends Component {
   render() {
     return (
