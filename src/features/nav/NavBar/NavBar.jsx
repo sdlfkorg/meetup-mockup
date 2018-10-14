86b9c7b11bc10bf5f9665c730060aad63d70f9c0
sdlfkorg <sdlfkorg@gmail.com>
2018-10-14 18:19:56 +0800

    set up navbar and fake auth function

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import {Menu, Container, Button} from 'semantic-ui-react';
 import {NavLink, Link} from 'react-router-dom';
+import SignOutMenu from '../Menu/SignOutMenu';
+import SignInedMenu from '../Menu/SignInedMenu';
 
 class NavBar extends Component {
+  state = {
+      authenticated: false
+  }
+
+  handleSignIn = () => {
+      this.setState({
+          authenticated: true
+      })
+  }
+  
+  handleSignOut = () => {
+    this.setState({
+        authenticated: false
+    })
+}
+
   render() {
+    const {authenticated} = this.state;
     return (
         <Menu inverted fixed="top">
             <Container>
@@ -16,10 +35,9 @@ class NavBar extends Component {
             <Menu.Item>
                 <Button floated="right" as={Link} to="createEvent" positive inverted content="Create Event" />
             </Menu.Item>
-            <Menu.Item position="right">
-                <Button basic inverted content="Login" />
-                <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
-            </Menu.Item>
+            {authenticated ?  <SignInedMenu signOut={this.handleSignOut} /> : <SignOutMenu signIn={this.handleSignIn} />}
+            
+            
             </Container>
         </Menu>
     )
