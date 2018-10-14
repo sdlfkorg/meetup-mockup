5281cce18e6c39df0144a6e60ed36b872d05db99
sdlfkorg <sdlfkorg@gmail.com>
2018-10-14 21:46:40 +0800

    fix signout function and hide content need auth

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Menu, Container, Button} from 'semantic-ui-react';
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink, Link, withRouter} from 'react-router-dom';
 import SignOutMenu from '../Menu/SignOutMenu';
 import SignInedMenu from '../Menu/SignInedMenu';
 
@@ -19,6 +19,7 @@ class NavBar extends Component {
     this.setState({
         authenticated: false
     })
+    this.props.history.push('/');
 }
 
   render() {
@@ -27,14 +28,19 @@ class NavBar extends Component {
         <Menu inverted fixed="top">
             <Container>
             <Menu.Item as={Link} to="/" header>
-                <img src="assets/logo.png" alt="logo" />
+                <img src="/assets/logo.png" alt="logo" />
                 Re-vents
             </Menu.Item>
             <Menu.Item as={NavLink} to='/events' name="Events" />
-            <Menu.Item as={NavLink} to='/people' name="People" />
-            <Menu.Item>
-                <Button floated="right" as={Link} to="createEvent" positive inverted content="Create Event" />
-            </Menu.Item>
+
+            {authenticated && 
+                <Menu.Item as={NavLink} to='/people' name="People" />
+            }
+            {authenticated &&
+                <Menu.Item>
+                    <Button floated="right" as={Link} to="/createEvent" positive inverted content="Create Event" />
+                </Menu.Item>
+            }
             {authenticated ?  <SignInedMenu signOut={this.handleSignOut} /> : <SignOutMenu signIn={this.handleSignIn} />}
             
             
@@ -43,4 +49,4 @@ class NavBar extends Component {
     )
   }
 }
-export default NavBar;
+export default withRouter(NavBar);
