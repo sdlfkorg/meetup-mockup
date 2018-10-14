601b96eddcbed8ba1314b27c2a003463cd9be6b1
sdlfkorg <sdlfkorg@gmail.com>
2018-10-14 21:47:52 +0800

    set up setting dashboard and setting nav

--- src/features/nav/Menu/SignInedMenu.jsx
+++ src/features/nav/Menu/SignInedMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Menu, Dropdown, Image} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 const SignInedMenu = ({signOut}) => {
   return (
@@ -11,7 +12,7 @@ const SignInedMenu = ({signOut}) => {
               <Dropdown.Item text="My Events" icon="calendar" />
               <Dropdown.Item text="My Network" icon="users" />
               <Dropdown.Item text="My Profile" icon="user" />
-              <Dropdown.Item text="Settings" icon="settings" />
+              <Dropdown.Item text="Settings" icon="settings" as={Link} to="/settings" />
               <Dropdown.Item onClick={signOut} text="Sign Out" icon="power" />
             </Dropdown.Menu>
           </Dropdown>
--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import {Grid} from 'semantic-ui-react';
+import SettingsNav from './SettingsNav';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import BasicPage from './BasicPage';
+import AboutPage from './AboutPage';
+import PhotoPage from './PhotosPage';
+import AccountPage from './AccountPage';
 
 class SettingsDashboard extends Component {
   render() {
     return (
-      <div>
+      <Grid>
+        <Grid.Column width={12}>
+          
+          <Switch>
+            <Redirect exact from="/settings" to="/settings/basic" />
+            <Route path='/settings/basic' component={BasicPage} />
+            <Route path="/settings/about" component={AboutPage} />
+            <Route path="/settings/photo" component={PhotoPage} />
+            <Route path="/settings/account" component={AccountPage} />
+          </Switch>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <h1>Nav</h1>
+          <SettingsNav/>
+        </Grid.Column>
         
-      </div>
+      </Grid>
     )
   }
 }
