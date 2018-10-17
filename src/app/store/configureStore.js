51f94e97ec0a50ecbc539612f1ee0ae712622bd0
sdlfkorg <sdlfkorg@gmail.com>
2018-10-17 21:20:15 +0800

    setup hot module for reducer

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducer/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const configureStore = (preloadedState) => {
   const middlewares = [];
@@ -7,12 +8,23 @@ export const configureStore = (preloadedState) => {
 
   const storeEnhancer = [middlewareEnhancer];
 
-  const composedEnhancer = compose(...storeEnhancer);
+  const composedEnhancer = composeWithDevTools(...storeEnhancer);
 
   const store = createStore(
     rootReducer,
     preloadedState,
     composedEnhancer
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducer/rootReducer', () => {
+        const newRootReducer = require('../reducer/rootReducer').default;
+        store.replaceReducer(newRootReducer);
+      })
+    }
+  }
+
+
   return store;
 }
