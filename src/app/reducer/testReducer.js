eb9669b73495fb614ac43fcbdd364671bd06fa2d
sdlfkorg <sdlfkorg@gmail.com>
2018-10-20 18:15:14 +0800

    setup test area

--- src/app/reducer/testReducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-  data: 'test rararara'
-}
-
-const testReducer = (state = initialState, action) => {
-  return state;
-}
-
-export default testReducer;
--- src/features/testarea/testComponent.jsx
+++ src/features/testarea/testComponent.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Button} from 'semantic-ui-react';
+import {incrementCounter, decrementCounter} from './testActions'
 
 const mapState = (state) => {
   return {data: state.test.data};
+}
+
+const actions = {
+  incrementCounter, 
+  decrementCounter
 }
 
 class testComponent extends Component {
@@ -11,9 +18,11 @@ class testComponent extends Component {
       <div>
         <h1>Test Area</h1>
         <h2>test data is: {this.props.data}</h2>
+        <Button onClick={this.props.incrementCounter} color="green" content="+1" />
+        <Button onClick={this.props.decrementCounter} color="red" content="-1"/>
       </div>
     )
   }
 }
 
-export default connect(mapState)(testComponent)
+export default connect(mapState, actions)(testComponent)
