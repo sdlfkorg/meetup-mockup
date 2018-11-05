eb7e9353c95828acb739972cedba1e6f6936f47c
sdlfkorg <sdlfkorg@gmail.com>
2018-11-05 21:32:31 +0800

    replace locatino field of form

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,6 +10,7 @@ import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
 import SelectInput from "../../../app/common/form/SelectInput";
 import DateInput from "../../../app/common/form/DateInput";
+import PlaceInput from "../../../app/common/form/PlaceInput";
 
 const actions = {
   createEvent,
@@ -99,13 +100,15 @@ class EventForm extends Component {
               <Field
                 name="city"
                 type="text"
-                component={TextInput}
+                component={PlaceInput}
+                options={{type:['(cities)']}}
                 placeholder="Event city"
               />
               <Field
                 name="venue"
                 type="text"
-                component={TextInput}
+                component={PlaceInput}
+                options={{type:['(establishment)']}}
                 placeholder="Event venue"
               />
               <Field
